a80f0155a13be15ed95080f464903e3d2ebeb92f
nismaira <B122310454@student.utem.edu.my>
2025-06-23 22:49:40 +0800

    WEEK 2 done

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
@@ -6,62 +7,27 @@ const port = 3000;
 
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
-let collection;
+// Validation schema using Joi
+const rideSchema = Joi.object({
+    driver: Joi.string().required(),
+    passenger: Joi.string().required(),
+    pickup: Joi.string().required(),
+    destination: Joi.string().required(),
+    price: Joi.number().required()
+});
 
-async function connectDB() {
-    try {
-        await client.connect();
-        console.log("Connected to MongoDB");
-        const db = client.db("mytaxi");
-        collection = db.collection("rides");
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-connectDB();
 
 app.post('/rides', async (req, res) => {
     try {
+        // Validate request body
+        const { error } = rideSchema.validate(req.body);
+        if (error) {
+            return res.status(400).send({ error: error.details[0].message });
+        }
+
         const result = await collection.insertOne(req.body);
         res.send(result);
     } catch (err) {
         res.status(500).send(err);
     }
-});
-
-app.get('/rides', async (req, res) => {
-    try {
-        const rides = await collection.find().toArray();
-        res.send(rides);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-app.put('/rides/:id', async (req, res) => {
-    try {
-        const result = await collection.updateOne(
-            { _id: new ObjectId(req.params.id) },
-            { $set: req.body }
-        );
-        res.send(result);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-app.delete('/rides/:id', async (req, res) => {
-    try {
-        const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
-        res.send(result);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
 });
